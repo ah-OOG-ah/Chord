ca5cd5a4b544b1e0540ebaf16c4ed936f72ba88a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-01-16 22:57:27 -0500

    Apply LGPLv3

--- adv_input/adv_boolput.m
+++ adv_input/adv_boolput.m
@@ -1,3 +1,19 @@
+% This file is part of Chord - scripts to ease MATLAB's dissonance.
+% Copyright (C) 2024 ah-OOG-ah
+%
+% Chord is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Chord is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 % Boolean input
 function ret = adv_boolput(prompt)
 
